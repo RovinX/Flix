47c73acb6530fa9ec5ec41d351004716a1644534
RODOLFO PUNZALAN <1011904529@wce111-12.local>
2019-01-31 20:28:48 -0800

    update: movie titles

--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -8,12 +8,34 @@
 
 import UIKit
 
-class MoviesViewController: UIViewController {
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        
+        cell.textLabel!.text = title
+        
+        return cell
+    }
+    
+    
+    @IBOutlet weak var tableView: UITableView!
     
     var movies = [[String:Any]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -26,6 +48,8 @@ class MoviesViewController: UIViewController {
                 let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
                 
                 self.movies = dataDictionary["results"] as! [[String:Any]]
+                
+                self.tableView.reloadData()
                 
                 print(dataDictionary)
                 // TODO: Get the array of movies
